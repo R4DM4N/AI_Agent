ec99a937d9d6c945a311aeee530cbddc2eb69b50
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-12-06 23:19:15 +1000

    Main.py is updated and running successefully.  Request broken in to seporate function for flexibilty of changing promps and llm on the fly.

--- main.py
+++ main.py
@@ -1,36 +1,80 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-from google.genai import types
+
+# from google.genai import types
 from datetime import datetime
 
-def main():
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    if api_key is None:
-        raise Run timeError("api_key not found")
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-        usage_metadata=GeneratContentResponseUsageMetadata
-    )
+MODEL_ID = "gemini-2.5-flash"  # @param ["gemini-2.5-flash-lite", "gemini-2.5-flash", "gemini-2.5-pro","gemini-3-pro-preview"] {"allow-input":true, isTemplate: true}
 
-    #print(response)
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY environment variable not set")
+client = genai.Client(api_key=api_key)
+
+prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+
+def main():
+    # Token Max
+    t_max = token_max(MODEL_ID)
+    print(t_max)
+    # Count tokens
+
+    response = prompt_request(MODEL_ID, prompt)
+    response_tokens = 0
+    prompt_tokens = 0
+    metadata = response.usage_metadata.candidates_token_count
+    if metadata is not None:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+    else:
+        print("Metadata returned None, there is no token count this time.")
+
+    # print(response)
+    print("User propmt:", prompt)
+    print("Prompt tokens: ", prompt_tokens)
+    print("Response tokens: ", response_tokens)
+    print("Response:")
     print(response.text)
+
 
 def get_current_timestamp():
     return datetime.now().isoformat() + "Z"
 
 
 def chat_with_gemini(user_input):
-        if usage_metadata is not None:
-            usage_metadata = {
-            "usage_token_count": 0, # Initialize
+    if user_input.usage_metadata is not None:
+        usage_metadata = {
+            "usage_token_count": 0,  # Initialize
             "timestamp": get_current_timestamp(),
         }
-    
-main()
+    return usage_metadata
+
+
+def token_max(AI_model):
+    model_info = client.models.get(model=AI_model)  # """model"""
+    prompt_max = model_info.input_token_limit
+    response_max = model_info.output_token_limit
+
+    print("Context window:", prompt_max, "tokens")
+    print("Max output window:", response_max, "tokens")
+
+    return (prompt_max, response_max)
+
+
+def token_count(model, prompt):
+    prompt_response = client.models.count_tokens(
+        model=model,
+        contents=prompt,
+    )
+    return prompt_response
+
+
+def prompt_request(model, prompt):
+    response = client.models.generate_content(model=model, contents=prompt)
+    return response
 
 
 if __name__ == "__main__":
