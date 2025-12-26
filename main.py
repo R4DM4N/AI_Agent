2609a3714c71c09a0e7adb223dc26b569101bd57
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-12-26 19:12:29 +1000

    message type added to the llm for more complex prompts/conversations passed.

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 def main():
     parser = argparse.ArgumentParser(description="AI Code Assistant")
     parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
     args = parser.parse_args()
-
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
@@ -18,7 +18,8 @@ def main():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages,
+        #contents=args.user_prompt,
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
